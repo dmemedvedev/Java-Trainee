a3de910face00613db1496ba8198fe74fad75a86
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2022-12-18 22:07:31 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int w =3;
+        int h =4;
+        System.out.println(w*2+h*2);
+        System.out.println(w*h);
+    }
+}
